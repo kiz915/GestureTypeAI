87706d14a26689bd19d3a8695ed592b3642c0507
kiz915 <kishorevk40@gmail.com>
2026-04-24 15:43:53 +0530

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,6 @@
+# GestureType AI Modules
+from .gesture_recognizer import GestureRecognizer
+from .sign_language import SignLanguageDetector
+from .pc_controller import PCController
+from .virtual_keyboard import VirtualKeyboard
 
