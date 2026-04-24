9d0fd9b819abb0badf5a99b163006d4fe82ea4fb
kiz915 <kishorevk40@gmail.com>
2026-04-24 19:48:14 +0530

    Delete modules directory

--- modules/__init__.py
+++
@@ -1,6 +0,0 @@
-# GestureType AI Modules
-from .gesture_recognizer import GestureRecognizer
-from .sign_language import SignLanguageDetector
-from .pc_controller import PCController
-from .virtual_keyboard import VirtualKeyboard
-
